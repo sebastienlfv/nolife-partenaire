e51c97f2942c8bb8b13dc29334d2bdf9b2de8919
sebastienlfv <sebastienlfv.pro@gmail.com>
2023-01-24 17:10:17 +0100

    email sender

--- app.js
+++ app.js
@@ -67,11 +67,32 @@ app.post('/send-email', function(req, res) {
     from: email,
     to: 'bnz@nolife-clothing.fr',
     subject: 'Demandes de partenariat - ' + firstname,
-    text: `Prénom: ${firstname}, Nom: ${lastname}, Email: ${email}, Genre: ${genre}, Date de naissance: ${date},
-    Site: ${website}, Téléphone: ${phone}, Adresse: ${adress}, Qui sont vos sponsors actuels ?: ${sponsor}, Comment promouvoir la marque Nolife ?: ${nolife},
-    Qu'est ce qui fait de vous un bon candidat pour un partenariat: ${goodPartner}, Quel type de partenariat recherchez vous ?: ${whatPartner},
-    Quel est votre jeu principal ?: ${jeux}, Sur quelles plateformes diffusez-vous ?: ${plateform}, Lien twitter: ${linkTwitter}, Lien twitch: ${linkTwitch}, Lien facebook: ${linkFacebook},
-    Lien instagram: ${linkInstagram}, Lien youtube: ${linkYoutube}, Lien tiktok: ${linkTiktok}`
+    // text: `Prénom: ${firstname}, Nom: ${lastname}, Email: ${email}, Genre: ${genre}, Date de naissance: ${date},
+    // Site: ${website}, Téléphone: ${phone}, Adresse: ${adress}, Qui sont vos sponsors actuels ?: ${sponsor}, Comment promouvoir la marque Nolife ?: ${nolife},
+    // Qu'est ce qui fait de vous un bon candidat pour un partenariat: ${goodPartner}, Quel type de partenariat recherchez vous ?: ${whatPartner},
+    // Quel est votre jeu principal ?: ${jeux}, Sur quelles plateformes diffusez-vous ?: ${plateform}, Lien twitter: ${linkTwitter}, Lien twitch: ${linkTwitch}, Lien facebook: ${linkFacebook},
+    // Lien instagram: ${linkInstagram}, Lien youtube: ${linkYoutube}, Lien tiktok: ${linkTiktok}`,
+    html: `<h1 style="font-size: 25px; color: #000">Demandes de partenariat - ${firstname}</h1> </br> 
+    <p style="font-weight: bold; margin-top: 20px">Nom : <span style="font-weight: normal">${lastname}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 10px">Prénom : <span style="font-weight: normal">${firstname}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Email : <span style="font-weight: normal">${email}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Genre : <span style="font-weight: normal">${genre}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Date de naissance : <span style="font-weight: normal">${date}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Site web : <span style="font-weight: normal">${website}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Téléphone : <span style="font-weight: normal">${phone}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Adresse : <span style="font-weight: normal">${adress}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Qui sont vos sponsors actuels ? : <span style="font-weight: normal">${sponsor}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Comment promouvoir la marque Nolife ? : <span style="font-weight: normal">${nolife}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Qu'est ce qui fait de vous un bon candidat pour un partenariat ? : <span style="font-weight: normal">${goodPartner}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Quel type de partenariat recherchez vous ? : <span style="font-weight: normal">${whatPartner}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Quel est votre jeu principal ? : <span style="font-weight: normal">${jeux}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Sur quelles plateformes diffusez-vous ? : <span style="font-weight: normal">${plateform}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Lien twitter : <span style="font-weight: normal">${linkTwitter}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Lien twitch : <span style="font-weight: normal">${linkTwitch}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Lien facebook : <span style="font-weight: normal">${linkFacebook}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Lien instagram : <span style="font-weight: normal">${linkInstagram}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Lien youtube : <span style="font-weight: normal">${linkYoutube}</span></p> </br>
+    <p style="font-weight: bold; margin-top: 20px">Lien tiktok : <span style="font-weight: normal">${linkTiktok}</span></p> </br>`
   }
   transporter.sendMail(mailOptions, function(err, info) {
     if(err) {
